1110822db12325f14d474a216f2e2966822dadb4
Brayan Guerrero <brayanstewartguor@gmail.com>
2026-05-01 18:50:17 -0500

    refactor(entity): remove unused @Index annotations in TechnologyCapacityEntity

--- infrastructure/driven-adapters/r2dbc-repository/src/main/java/co/com/technology/r2dbc/technologycapacity/entity/TechnologyCapacityEntity.java
+++ infrastructure/driven-adapters/r2dbc-repository/src/main/java/co/com/technology/r2dbc/technologycapacity/entity/TechnologyCapacityEntity.java
@@ -7,7 +7,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Column;
-import org.springframework.data.relational.core.mapping.Index;
 import org.springframework.data.relational.core.mapping.Table;
 
 @Getter
@@ -21,10 +20,8 @@ public class TechnologyCapacityEntity {
     private Long id;
 
     @Column("technology_id")
-    @Index("idx_technology_id")
     private Long technologyId;
 
     @Column("capacity_external_id")
-    @Index("idx_capacity_external_id")
     private Long capacityExternalId;
 }
